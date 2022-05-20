f1c8bd32fb09ba389a5e416c7e5bc7d9cbfdad03
aetotvl6789 <nguyenviethuanlove043@gmail.com>
2022-05-21 00:20:58 +0700

    Delete src/macosMain directory

--- src/macosMain/kotlin/com/jetbrains/handson/handson/init/ConfigurationException.kt
+++
@@ -1,3 +0,0 @@
-package com.jetbrains.handson.handson.init
-
-class ConfigurationException(override val message: String) : Throwable(message)
--- src/macosMain/kotlin/com/jetbrains/handson/handson/init/FileIOException.kt
+++
@@ -1,6 +0,0 @@
-package com.jetbrains.handson.handson.init
-
-import kotlinx.cinterop.toKString
-import platform.posix.strerror
-
-class FileIOException(errorCode: Int) : Throwable(strerror(errorCode)?.toKString() ?: "Error: $errorCode")
